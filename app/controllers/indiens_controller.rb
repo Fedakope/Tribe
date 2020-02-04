43c4bd8a62c4d8a22b7caefd33a35fb05e8f6f30
Nil <nilbesombes@gmail.com>
2020-02-04 02:51:28 +0100

    ok index/create

--- app/controllers/indiens_controller.rb
+++ app/controllers/indiens_controller.rb
@@ -1,14 +1,31 @@
 class IndiensController < ApplicationController
   def index
-    @indiens = Indiens.all
+    @indiens = Indien.all
   end
 
   def show
+    @indien = Indien.find(params[:id])
   end
 
   def new
+    @indien = Indien.new
+  end
+
+  def create
+    @indien = Indien.new(indien_params)
+    @indien.save
+
+    redirect_to indien_path(@indien)
   end
 
   def edit
   end
+
+  private
+
+  # Useless les params car on permet tout
+  def indien_params
+    params.require(:indien).permit(:name, :surname, :birthdate, :ancestor, :longitude, :latitude)
+  end
+
 end
