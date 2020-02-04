5e5a16102f9cced588ec82a36d1501f1bd119a26
Nil <nilbesombes@gmail.com>
2020-02-04 11:26:52 +0100

    ok age moyen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,27 @@
 class PagesController < ApplicationController
   def statistiques
-  end
+
+  #  <% dob = Date.new(1990, 05, 04) %>
+  #  <% age = Date.today.year - dob.year %>
+  # <% age -= 1 if Date.today < dob + age.years %>
+  #  Age (dob 1990 05 04)= <%= age %>
+
+    allAge = []
+    @indiens = Indien.all
+
+    @indiens.each do |indien|
+     indien.birthdate.year
+     age = Date.today.year - indien.birthdate.year
+      age2 = age - 1 if Date.today < indien.birthdate + age.years
+      allAge << age2
+
+      end
+      
+      @ageMoyen = allAge.sum.fdiv(allAge.size).round(1)
+
+
+  end 
+
+
+
 end
